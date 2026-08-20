1ee2e64cf93be1f2f66e3fc255faedf3a188f6fc
Mim099 <debosmitablogs@gmail.com>
2026-08-21 02:22:12 +0530

    Add per-song listener tracking

--- server.js
+++ server.js
@@ -21,6 +21,8 @@ const io = new Server(server, {
 });
 
 let listeners = 0;
+let songCounts = {}; // { songId: countOfPeopleListening }
+let socketSongs = {}; // { socketId: songId }
 
 app.get("/", (req, res) => {
   console.log("Root route hit!");
@@ -32,23 +34,57 @@ app.get("/", (req, res) => {
 
 io.on("connection", (socket) => {
   listeners++;
-
   console.log("Listener connected:", socket.id);
   console.log("Current listeners:", listeners);
-
   io.emit("listener-count", listeners);
+
+  socket.on("song-playing", (songId) => {
+    // If this socket was already counted for a different song, remove it from there first
+    const previousSong = socketSongs[socket.id];
+    if (previousSong && songCounts[previousSong]) {
+      songCounts[previousSong]--;
+      if (songCounts[previousSong] <= 0) {
+        delete songCounts[previousSong];
+      }
+    }
+
+    socketSongs[socket.id] = songId;
+    songCounts[songId] = (songCounts[songId] || 0) + 1;
+
+    io.emit("song-counts", songCounts);
+  });
+
+  socket.on("song-paused", () => {
+    const currentSong = socketSongs[socket.id];
+    if (currentSong && songCounts[currentSong]) {
+      songCounts[currentSong]--;
+      if (songCounts[currentSong] <= 0) {
+        delete songCounts[currentSong];
+      }
+    }
+
+    delete socketSongs[socket.id];
+    io.emit("song-counts", songCounts);
+  });
 
   socket.on("disconnect", () => {
     listeners--;
-
     if (listeners < 0) {
       listeners = 0;
     }
-
     console.log("Listener disconnected:", socket.id);
     console.log("Current listeners:", listeners);
-
     io.emit("listener-count", listeners);
+
+    const currentSong = socketSongs[socket.id];
+    if (currentSong && songCounts[currentSong]) {
+      songCounts[currentSong]--;
+      if (songCounts[currentSong] <= 0) {
+        delete songCounts[currentSong];
+      }
+    }
+    delete socketSongs[socket.id];
+    io.emit("song-counts", songCounts);
   });
 });
 
